5d78b6946e2b22ef55ecec82e9378702ec33e9a6
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2022-01-11 11:12:11 +0300

    Add files via upload

---
+++ project_6.js
@@ -0,0 +1,23 @@
+//Beni Yaratan Elbet Yolumu Gösterir.Suara /78.
+// This program written for Sum,Extract,Multipication and Division change variable  at Js.
+number_1 = 21
+number_2 = 7
+sum = number_1 +number_2
+console.log(sum)
+exttract = number_1 -number_2
+console.log(exttract)
+multipication = number_1 * number_2
+console.log(multipication)
+division = number_1 / number_2
+console.log (division)
+//We are changing values of number_1 and number_2 and write same operations codes .
+number_1 = 12
+number_2 = 4
+sum = number_1 +number_2
+console.log(sum)
+exttract = number_1 -number_2
+console.log(exttract)
+multipication = number_1 * number_2
+console.log(multipication)
+division = number_1 / number_2
+console.log (division)
